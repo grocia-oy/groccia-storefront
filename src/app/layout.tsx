3c3ed4099b23ec6325ece2f172534de25bfaecaf
Nguyen Pham <nguyenfamj1409@gmail.com>
2024-01-25 22:51:43 +0200

    feat: typography configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,39 @@
 import { Metadata } from "next"
+import localFont from "next/font/local"
 import "styles/globals.css"
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "https://localhost:8000"
+
+// Fonts
+const raleway = localFont({
+  src: [
+    {
+      path: "../../public/fonts/Raleway/Raleway400.ttf",
+      weight: "400",
+    },
+    {
+      path: "../../public/fonts/Raleway/Raleway600.ttf",
+      weight: "600",
+    },
+  ],
+  variable: "--font-raleway",
+})
+
+const roboto = localFont({
+  src: [
+    {
+      path: "../../public/fonts/Roboto/Roboto400.ttf",
+      weight: "400",
+    },
+    { path: "../../public/fonts/Roboto/Roboto500.ttf", weight: "500" },
+  ],
+  variable: "--font-roboto",
+})
+
+const gotag = localFont({
+  src: [{ path: "../../public/fonts/Gotag/Gotag.ttf", weight: "400" }],
+  variable: "--font-gotag",
+})
 
 export const metadata: Metadata = {
   metadataBase: new URL(BASE_URL),
@@ -9,9 +41,13 @@ export const metadata: Metadata = {
 
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
-    <html lang="en" data-mode="light">
+    <html
+      lang="en"
+      data-mode="light"
+      className={`${raleway.variable} ${roboto.variable} ${gotag.variable}`}
+    >
       <body>
-        <main className="relative">{props.children}</main>
+        <main className="relative font-roboto">{props.children}</main>
       </body>
     </html>
   )
