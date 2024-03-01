d9d836d5cdcee42fa4855e4160b35c3c1e6d0a14
Nguyen Pham <nguyenfamj1409@gmail.com>
2024-03-02 00:37:01 +0200

    ci: New rules for eslint with prettier integration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,9 @@
 module.exports = {
-  extends: ["next/core-web-vitals"]
+  extends: ['next/core-web-vitals', 'prettier'],
+  plugins: ['plugin:@typescript-eslint/recommended', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
+  },
 };
