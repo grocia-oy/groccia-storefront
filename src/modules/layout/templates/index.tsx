6aaa731523e31c8f06783949a266f371686b3466
Nguyen Pham <nguyenfamj1409@gmail.com>
2024-03-01 23:50:49 +0200

    refactor: Changing prop access

--- src/modules/layout/templates/index.tsx
+++ src/modules/layout/templates/index.tsx
@@ -1,15 +1,16 @@
 import React from "react"
-
+import AnnouncementBar from "@modules/layout/components/announcement-bar"
 import Footer from "@modules/layout/templates/footer"
 import Nav from "@modules/layout/templates/nav"
 
 const Layout: React.FC<{
   children: React.ReactNode
-}> = ({ children }) => {
+}> = (props) => {
   return (
     <div>
+      <AnnouncementBar/>
       <Nav />
-      <main className="relative">{children}</main>
+      <main className="relative">{props.children}</main>
       <Footer />
     </div>
   )
