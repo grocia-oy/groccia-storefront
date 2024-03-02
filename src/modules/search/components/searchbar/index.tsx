dc3b64ac323e823f95ef7f62a9da65e20d74f73f
Nguyen Pham <nguyenfamj1409@gmail.com>
2024-03-03 00:00:46 +0200

    feat: Modify searchbar styles

--- src/modules/search/components/searchbar/index.tsx
+++ src/modules/search/components/searchbar/index.tsx
@@ -1,12 +1,12 @@
-"use client"
-import React, { useState, FormEvent } from "react"
+'use client';
+import React, { useState, FormEvent } from 'react';
 
 const SearchBar: React.FC = () => {
-  const [searchTerm, setSearchTerm] = useState<string>("")
+  const [searchTerm, setSearchTerm] = useState<string>('');
 
   const handleSearch = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-  }
+    event.preventDefault();
+  };
 
   return (
     <form onSubmit={handleSearch} className="w-full flex items-center relative">
@@ -15,15 +15,17 @@ const SearchBar: React.FC = () => {
         data-ms-editor
         spellCheck={false}
         placeholder="Search products..."
-        className="w-full px-4 py-3 border rounded-2xl bg-gray-100"
+        className="w-full pl-3 pr-20 py-3 rounded-2xl text-sm bg-neutral placeholder:text-input-placeholder focus:outline-none focus:border-none focus:ring-0 appearance-none"
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
       />
-      <div className="absolute right-3">
-        <button className=" h-full px-2 py-1 bg-slate-700 text-secondary rounded-lg">Search</button>
+      <div className="absolute right-0 h-full">
+        <button type='submit' className="h-full px-3 bg-primary-700 text-sm text-white rounded-2xl">
+          Search
+        </button>
       </div>
     </form>
-  )
-}
+  );
+};
 
-export default SearchBar
+export default SearchBar;
