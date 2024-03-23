d8b09b04b247d0385c4f259441e1b80c56bcb8c8
Nguyen Pham <nguyenfamj1409@gmail.com>
2024-03-23 20:46:46 +0200

    feat: Update product component style from boilerplate

--- src/modules/home/components/featured-products/product-rail/index.tsx
+++ src/modules/home/components/featured-products/product-rail/index.tsx
@@ -1,43 +1,47 @@
-import { Region } from "@medusajs/medusa"
-import { Text } from "@medusajs/ui"
+import { Region } from '@medusajs/medusa';
+import CarouselSlide from '@modules/common/components/carousel/carousel-slide';
 
-import InteractiveLink from "@modules/common/components/interactive-link"
-import ProductPreview from "@modules/products/components/product-preview"
-import { ProductCollectionWithPreviews } from "types/global"
+import InteractiveLink from '@modules/common/components/interactive-link';
+import ProductPreview from '@modules/products/components/product-preview';
+import { ProductCollectionWithPreviews } from 'types/global';
 
 export default function ProductRail({
   collection,
   region,
 }: {
-  collection: ProductCollectionWithPreviews
-  region: Region
+  collection: ProductCollectionWithPreviews;
+  region: Region;
 }) {
-  const { products } = collection
+  const { products } = collection;
 
   if (!products) {
-    return null
+    return null;
   }
 
   return (
-    <div className="content-container py-12 small:py-24">
-      <div className="flex justify-between mb-8">
-        <Text className="txt-xlarge">{collection.title}</Text>
+    <div className="content-container mt-6">
+      <div className="flex justify-between mb-4 ">
+        <h2 className="font-raleway font-bold text-xl text-primary-default">{collection.title}</h2>
         <InteractiveLink href={`/collections/${collection.handle}`}>
           View all
         </InteractiveLink>
       </div>
-      <ul className="grid grid-cols-2 small:grid-cols-3 gap-x-6 gap-y-24 small:gap-y-36">
+
+      <CarouselSlide chevronNavigation>
         {products &&
           products.map((product) => (
-            <li key={product.id}>
+            <div
+              key={product.id}
+              className="flex-carousel-slide sm:flex-carousel-slide-sm lg:flex-carousel-slide-lg xl:flex-carousel-slide-xl min-w-0"
+            >
               <ProductPreview
                 productPreview={product}
                 region={region}
                 isFeatured
               />
-            </li>
+            </div>
           ))}
-      </ul>
+      </CarouselSlide>
     </div>
-  )
+  );
 }
