8e78931e8dc5f83a1a1379ef2dde38ba2e4212e4
Nguyen Pham <nguyenfamj1409@gmail.com>
2024-03-25 00:41:48 +0200

    feat: localized link for server side

---
+++ src/modules/common/components/localized-server-link/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+/**
+ * Use this component to create a Next.js `<Link />` that persists the current country code in the url,
+ * without having to explicitly pass it as a prop.
+ */
+const LocalizedServerLink = ({
+  lang,
+  locale,
+  children,
+  linkProps,
+}: {
+  lang: string;
+  locale: string;
+  children: string;
+  linkProps: React.DetailedHTMLProps<
+    React.AnchorHTMLAttributes<HTMLAnchorElement>,
+    HTMLAnchorElement
+  >;
+}) => {
+  const { href, ...props } = linkProps;
+  const correctedHref = href?.startsWith('http')
+    ? href
+    : `/${lang}/${locale}${href}`;
+
+  return (
+    <a href={correctedHref} {...props}>
+      {children}
+    </a>
+  );
+};
+
+export default LocalizedServerLink;
