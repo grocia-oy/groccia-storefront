b7fdeaef0a1d571e984b4837c02867047f85947a
Nguyen Pham <nguyenfamj1409@gmail.com>
2024-03-25 00:38:45 +0200

    feat: strapi footer and type updates

--- src/types/strapi.ts
+++ src/types/strapi.ts
@@ -1,40 +1,48 @@
+import { HTMLAttributeAnchorTarget } from 'react';
+
 // Base type for all Strapi API type
 export interface StrapiBaseType {
-  id: number
-  createdAt: string
-  updatedAt: string
-  publishedAt?: string
-  locale?: string
-  meta?: Record<string, string>
+  id: number;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt?: string;
+  locale?: string;
+  meta?: Record<string, string>;
 }
 type StrapiMediaFormat = {
-  ext: string
-  url: string
-  hash: string
-  mime: string
-  name: string
-  path: string
-  size: number
-  width: number
-  height: number
-}
+  ext: string;
+  url: string;
+  hash: string;
+  mime: string;
+  name: string;
+  path: string;
+  size: number;
+  width: number;
+  height: number;
+};
 
 export type StrapiMediaComponent = StrapiBaseType & {
-  name: string
-  alternativeText: string
-  caption: string
-  width: number
-  height: number
+  name: string;
+  alternativeText: string;
+  caption: string;
+  width: number;
+  height: number;
   formats?: {
-    small?: StrapiMediaFormat
-    thumbnail?: StrapiMediaFormat
-  }
-  hash: string
-  ext: string
-  mime: string
-  size: number
-  url: string
-  previewUrl: string
-  provider: string
-  provider_metadata: null
-}
+    small?: StrapiMediaFormat;
+    thumbnail?: StrapiMediaFormat;
+  };
+  hash: string;
+  ext: string;
+  mime: string;
+  size: number;
+  url: string;
+  previewUrl: string;
+  provider: string;
+  provider_metadata: null;
+};
+
+export type StrapiLinkComponent = {
+  url: string;
+  title: string;
+  target?: HTMLAttributeAnchorTarget;
+};
