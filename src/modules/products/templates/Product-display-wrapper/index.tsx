3e3646ff67f63f3e1747c25a27b932c1c0943625
Thang Phan <thang.phancao@aalto.fi>
2024-04-10 00:19:56 +0300

    fix: use a fallback image when there are no product images

--- src/modules/products/templates/Product-display-wrapper/index.tsx
+++ src/modules/products/templates/Product-display-wrapper/index.tsx
@@ -4,7 +4,6 @@ import { PricedProduct } from '@medusajs/medusa/dist/types/pricing';
 import { Container } from '@medusajs/ui';
 import ImageGallery from '@modules/products/components/image-gallery';
 import Image from 'next/image';
-import { notFound } from 'next/navigation';
 import { useState } from 'react';
 
 type ProductDisplayWrapperProps = {
@@ -12,32 +11,33 @@ type ProductDisplayWrapperProps = {
 };
 
 const ProductDisplayWrapper = ({ product }: ProductDisplayWrapperProps) => {
-  if (product.images?.length === 0) {
-    return notFound();
-  }
-
   const [displayedImageIndex, setDisplayedImageIndex] = useState(0);
 
   return (
-    <div className="relative mb-2 flex w-full lg:mb-0 lg:h-96 lg:w-3/5">
-      <ImageGallery
-        images={product?.images || []}
-        setDisplayedImageIndex={setDisplayedImageIndex}
-      />
-      <Container className="relative overflow-hidden">
-        {product?.images && (
-          <Image
-            src={product.images[displayedImageIndex].url}
-            className="relative bg-ui-bg-subtle"
-            fill
-            alt="Image"
-            style={{
-              objectFit: 'contain',
-            }}
-          />
-        )}
-      </Container>
-    </div>
+    <>
+      <div className="relative mb-2 flex h-52 w-full lg:mb-0 lg:h-96 lg:w-3/5">
+        <ImageGallery
+          images={product?.images || []}
+          setDisplayedImageIndex={setDisplayedImageIndex}
+        />
+        <Container className="relative overflow-hidden">
+          {product?.images && (
+            <Image
+              src={
+                product.images[displayedImageIndex]?.url ||
+                '/images/fallback.png'
+              }
+              className="relative bg-ui-bg-subtle"
+              fill
+              alt="Image"
+              style={{
+                objectFit: 'contain',
+              }}
+            />
+          )}
+        </Container>
+      </div>
+    </>
   );
 };
 
