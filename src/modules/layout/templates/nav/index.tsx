b02170a6f5346040eb5392e5d6d2e30c58560122
Thang Phan <thang.phancao@aalto.fi>
2024-04-10 18:59:11 +0300

    refactor: refactor PostcodeButtonModel component

--- src/modules/layout/templates/nav/index.tsx
+++ src/modules/layout/templates/nav/index.tsx
@@ -1,17 +1,14 @@
-'use client';
-import { Suspense, useRef } from 'react';
+import { Suspense } from 'react';
 import LocalizedClientLink from '@modules/common/components/localized-client-link';
 import SearchBar from '@modules/search/components/searchbar';
-import { ShoppingCartIcon } from '@heroicons/react/24/outline';
 import PostcodeButtonModal from '@modules/layout/components/postcode-button-modal';
 import User from '@modules/common/icons/user';
+import CartButton from '@modules/layout/components/cart-button';
 import AnimatedHamburgerWrapper from '@modules/common/components/animated-hamburget-xmark-wrapper';
 
 type Props = {};
 
 export default function Nav({}: Props) {
-  const postalCodeModalRef = useRef<HTMLDialogElement>(null);
-
   return (
     <div className="sticky top-0 inset-x-0 z-50">
       <header className="relative h-20 bg-white border-b">
@@ -30,17 +27,22 @@ export default function Nav({}: Props) {
           </div>
           <div className="flex flex-1 basis-0 justify-end">
             <div className="flex items-center space-x-10 font-raleway">
-              <PostcodeButtonModal modalRef={postalCodeModalRef} />
+              <PostcodeButtonModal />
               <button className="flex cursor-pointer items-center">
                 <span className="mr-1">
                   <User className="w-5 h-5" />
                 </span>
                 <div className="hidden lg:block">Login</div>
               </button>
-              <Suspense>
-                <button>
-                  <ShoppingCartIcon className="w-6 h-6" />
-                </button>
+              <Suspense
+                fallback={
+                  <LocalizedClientLink
+                    className="hover:text-ui-fg-base flex gap-2"
+                    href="/cart"
+                  ></LocalizedClientLink>
+                }
+              >
+                <CartButton />
               </Suspense>
             </div>
           </div>
