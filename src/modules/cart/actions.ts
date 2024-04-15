25602eb51f3efa614d366c9b90c76d513be3e7d4
Thang Phan <phancaothng@gmail.com>
2024-04-15 22:15:19 +0300

    style: change quotation mark and add semi-colon

--- src/modules/cart/actions.ts
+++ src/modules/cart/actions.ts
@@ -1,9 +1,9 @@
-"use server"
+'use server';
 
-import { LineItem } from "@medusajs/medusa"
-import { omit } from "lodash"
-import { revalidateTag } from "next/cache"
-import { cookies } from "next/headers"
+import { Cart, LineItem } from '@medusajs/medusa';
+import { omit } from 'lodash';
+import { revalidateTag } from 'next/cache';
+import { cookies } from 'next/headers';
 
 import {
   addItem,
@@ -13,8 +13,8 @@ import {
   removeItem,
   updateCart,
   updateItem,
-} from "@lib/data/ecommerce"
-import { getRegion } from "app/actions"
+} from '@lib/data/ecommerce';
+import { getRegion } from 'app/actions';
 
 /**
  * Retrieves the cart based on the cartId cookie
@@ -24,48 +24,48 @@ import { getRegion } from "app/actions"
  * const cart = await getOrSetCart()
  */
 export async function getOrSetCart(countryCode: string) {
-  const cartId = cookies().get("_medusa_cart_id")?.value
-  let cart
+  const cartId = cookies().get('_medusa_cart_id')?.value;
+  let cart;
 
   if (cartId) {
-    cart = await getCart(cartId).then((cart) => cart)
+    cart = await getCart(cartId).then((cart) => cart);
   }
 
-  const region = await getRegion(countryCode)
+  const region = await getRegion(countryCode);
 
   if (!region) {
-    return null
+    return null;
   }
 
-  const region_id = region.id
+  const region_id = region.id;
 
   if (!cart) {
-    cart = await createCart({ region_id }).then((res) => res)
-    cart && cookies().set("_medusa_cart_id", cart.id)
-    revalidateTag("cart")
+    cart = await createCart({ region_id }).then((res) => res);
+    cart && cookies().set('_medusa_cart_id', cart.id);
+    revalidateTag('cart');
   }
 
   if (cart && cart?.region_id !== region_id) {
-    await updateCart(cart.id, { region_id })
-    revalidateTag("cart")
+    await updateCart(cart.id, { region_id });
+    revalidateTag('cart');
   }
 
-  return cart
+  return cart;
 }
 
 export async function retrieveCart() {
-  const cartId = cookies().get("_medusa_cart_id")?.value
+  const cartId = cookies().get('_medusa_cart_id')?.value;
 
   if (!cartId) {
-    return null
+    return null;
   }
 
   try {
-    const cart = await getCart(cartId).then((cart) => cart)
-    return cart
+    const cart = await getCart(cartId).then((cart) => cart);
+    return cart;
   } catch (e) {
-    console.log(e)
-    return null
+    console.log(e);
+    return null;
   }
 }
 
@@ -74,25 +74,25 @@ export async function addToCart({
   quantity,
   countryCode,
 }: {
-  variantId: string
-  quantity: number
-  countryCode: string
+  variantId: string;
+  quantity: number;
+  countryCode: string;
 }) {
-  const cart = await getOrSetCart(countryCode).then((cart) => cart)
+  const cart = await getOrSetCart(countryCode).then((cart) => cart);
 
   if (!cart) {
-    return "Missing cart ID"
+    return 'Missing cart ID';
   }
 
   if (!variantId) {
-    return "Missing product variant ID"
+    return 'Missing product variant ID';
   }
 
   try {
-    await addItem({ cartId: cart.id, variantId, quantity })
-    revalidateTag("cart")
+    await addItem({ cartId: cart.id, variantId, quantity });
+    revalidateTag('cart');
   } catch (e) {
-    return "Error adding item to cart"
+    return 'Error adding item to cart';
   }
 }
 
@@ -100,51 +100,51 @@ export async function updateLineItem({
   lineId,
   quantity,
 }: {
-  lineId: string
-  quantity: number
+  lineId: string;
+  quantity: number;
 }) {
-  const cartId = cookies().get("_medusa_cart_id")?.value
+  const cartId = cookies().get('_medusa_cart_id')?.value;
 
   if (!cartId) {
-    return "Missing cart ID"
+    return 'Missing cart ID';
   }
 
   if (!lineId) {
-    return "Missing lineItem ID"
+    return 'Missing lineItem ID';
   }
 
   if (!cartId) {
-    return "Missing cart ID"
+    return 'Missing cart ID';
   }
 
   try {
-    await updateItem({ cartId, lineId, quantity })
-    revalidateTag("cart")
+    await updateItem({ cartId, lineId, quantity });
+    revalidateTag('cart');
   } catch (e: any) {
-    return e.toString()
+    return e.toString();
   }
 }
 
 export async function deleteLineItem(lineId: string) {
-  const cartId = cookies().get("_medusa_cart_id")?.value
+  const cartId = cookies().get('_medusa_cart_id')?.value;
 
   if (!cartId) {
-    return "Missing cart ID"
+    return 'Missing cart ID';
   }
 
   if (!lineId) {
-    return "Missing lineItem ID"
+    return 'Missing lineItem ID';
   }
 
   if (!cartId) {
-    return "Missing cart ID"
+    return 'Missing cart ID';
   }
 
   try {
-    await removeItem({ cartId, lineId })
-    revalidateTag("cart")
+    await removeItem({ cartId, lineId });
+    revalidateTag('cart');
   } catch (e) {
-    return "Error deleting line item"
+    return 'Error deleting line item';
   }
 }
 
@@ -152,32 +152,32 @@ export async function enrichLineItems(
   lineItems: LineItem[],
   regionId: string
 ): Promise<
-  | Omit<LineItem, "beforeInsert" | "beforeUpdate" | "afterUpdateOrLoad">[]
+  | Omit<LineItem, 'beforeInsert' | 'beforeUpdate' | 'afterUpdateOrLoad'>[]
   | undefined
 > {
   // Prepare query parameters
   const queryParams = {
     ids: lineItems.map((lineItem) => lineItem.variant.product_id),
     regionId: regionId,
-  }
+  };
 
   // Fetch products by their IDs
-  const products = await getProductsById(queryParams)
+  const products = await getProductsById(queryParams);
 
   // If there are no line items or products, return an empty array
   if (!lineItems?.length || !products) {
-    return []
+    return [];
   }
 
   // Enrich line items with product and variant information
 
   const enrichedItems = lineItems.map((item) => {
-    const product = products.find((p) => p.id === item.variant.product_id)
-    const variant = product?.variants.find((v) => v.id === item.variant_id)
+    const product = products.find((p) => p.id === item.variant.product_id);
+    const variant = product?.variants.find((v) => v.id === item.variant_id);
 
     // If product or variant is not found, return the original item
     if (!product || !variant) {
-      return item
+      return item;
     }
 
     // If product and variant are found, enrich the item
@@ -185,10 +185,10 @@ export async function enrichLineItems(
       ...item,
       variant: {
         ...variant,
-        product: omit(product, "variants"),
+        product: omit(product, 'variants'),
       },
-    }
-  }) as LineItem[]
+    };
+  }) as LineItem[];
 
   return enrichedItems;
 }
