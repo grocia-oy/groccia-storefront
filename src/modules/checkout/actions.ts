797066a2d53e1330b96bec5a78933f2f5bd40ed5
Thang Phan <phancaothng@gmail.com>
2024-04-18 21:00:00 +0300

    fix: redirect to localized url

--- src/modules/checkout/actions.ts
+++ src/modules/checkout/actions.ts
@@ -1,6 +1,6 @@
-"use server"
+'use server';
 
-import { cookies, headers } from 'next/headers';
+import { cookies } from 'next/headers';
 
 import {
   addShippingMethod,
@@ -8,62 +8,62 @@ import {
   deleteDiscount,
   setPaymentSession,
   updateCart,
-} from "@lib/data/ecommerce"
-import { GiftCard, StorePostCartsCartReq } from "@medusajs/medusa"
-import { revalidateTag } from "next/cache"
-import { redirect } from "next/navigation"
+} from '@lib/data/ecommerce';
+import { GiftCard, StorePostCartsCartReq } from '@medusajs/medusa';
+import { revalidateTag } from 'next/cache';
+import { redirect } from 'next/navigation';
 
 export async function cartUpdate(data: StorePostCartsCartReq) {
-  const cartId = cookies().get("_medusa_cart_id")?.value
+  const cartId = cookies().get('_medusa_cart_id')?.value;
 
-  if (!cartId) return "No cartId cookie found"
+  if (!cartId) return 'No cartId cookie found';
 
   try {
-    await updateCart(cartId, data)
-    revalidateTag("cart")
+    await updateCart(cartId, data);
+    revalidateTag('cart');
   } catch (error: any) {
-    return error.toString()
+    return error.toString();
   }
 }
 
 export async function applyDiscount(code: string) {
-  const cartId = cookies().get("_medusa_cart_id")?.value
+  const cartId = cookies().get('_medusa_cart_id')?.value;
 
-  if (!cartId) return "No cartId cookie found"
+  if (!cartId) return 'No cartId cookie found';
 
   try {
     await updateCart(cartId, { discounts: [{ code }] }).then(() => {
-      revalidateTag("cart")
-    })
+      revalidateTag('cart');
+    });
   } catch (error: any) {
-    throw error
+    throw error;
   }
 }
 
 export async function applyGiftCard(code: string) {
-  const cartId = cookies().get("_medusa_cart_id")?.value
+  const cartId = cookies().get('_medusa_cart_id')?.value;
 
-  if (!cartId) return "No cartId cookie found"
+  if (!cartId) return 'No cartId cookie found';
 
   try {
     await updateCart(cartId, { gift_cards: [{ code }] }).then(() => {
-      revalidateTag("cart")
-    })
+      revalidateTag('cart');
+    });
   } catch (error: any) {
-    throw error
+    throw error;
   }
 }
 
 export async function removeDiscount(code: string) {
-  const cartId = cookies().get("_medusa_cart_id")?.value
+  const cartId = cookies().get('_medusa_cart_id')?.value;
 
-  if (!cartId) return "No cartId cookie found"
+  if (!cartId) return 'No cartId cookie found';
 
   try {
-    await deleteDiscount(cartId, code)
-    revalidateTag("cart")
+    await deleteDiscount(cartId, code);
+    revalidateTag('cart');
   } catch (error: any) {
-    throw error
+    throw error;
   }
 }
 
@@ -71,9 +71,9 @@ export async function removeGiftCard(
   codeToRemove: string,
   giftCards: GiftCard[]
 ) {
-  const cartId = cookies().get("_medusa_cart_id")?.value
+  const cartId = cookies().get('_medusa_cart_id')?.value;
 
-  if (!cartId) return "No cartId cookie found"
+  if (!cartId) return 'No cartId cookie found';
 
   try {
     await updateCart(cartId, {
@@ -81,10 +81,10 @@ export async function removeGiftCard(
         .filter((gc) => gc.code !== codeToRemove)
         .map((gc) => ({ code: gc.code })),
     }).then(() => {
-      revalidateTag("cart")
-    })
+      revalidateTag('cart');
+    });
   } catch (error: any) {
-    throw error
+    throw error;
   }
 }
 
@@ -92,15 +92,15 @@ export async function submitDiscountForm(
   currentState: unknown,
   formData: FormData
 ) {
-  const code = formData.get("code") as string
+  const code = formData.get('code') as string;
 
   try {
     await applyDiscount(code).catch(async (err) => {
-      await applyGiftCard(code)
-    })
-    return null
+      await applyGiftCard(code);
+    });
+    return null;
   } catch (error: any) {
-    return error.toString()
+    return error.toString();
   }
 }
 
@@ -115,100 +115,100 @@ export async function setAddresses(currentState: unknown, formData: FormData) {
 
   if (!formData) return 'No form data received';
 
-  const cartId = cookies().get("_medusa_cart_id")?.value
+  const cartId = cookies().get('_medusa_cart_id')?.value;
 
-  if (!cartId) return { message: "No cartId cookie found" }
+  if (!cartId) return { message: 'No cartId cookie found' };
 
   const data = {
     shipping_address: {
-      first_name: formData.get("shipping_address.first_name"),
-      last_name: formData.get("shipping_address.last_name"),
-      address_1: formData.get("shipping_address.address_1"),
-      address_2: "",
-      company: formData.get("shipping_address.company"),
-      postal_code: formData.get("shipping_address.postal_code"),
-      city: formData.get("shipping_address.city"),
-      country_code: formData.get("shipping_address.country_code"),
-      province: formData.get("shipping_address.province"),
-      phone: formData.get("shipping_address.phone"),
+      first_name: formData.get('shipping_address.first_name'),
+      last_name: formData.get('shipping_address.last_name'),
+      address_1: formData.get('shipping_address.address_1'),
+      address_2: '',
+      company: formData.get('shipping_address.company'),
+      postal_code: formData.get('shipping_address.postal_code'),
+      city: formData.get('shipping_address.city'),
+      country_code: formData.get('shipping_address.country_code'),
+      province: formData.get('shipping_address.province'),
+      phone: formData.get('shipping_address.phone'),
     },
-    email: formData.get("email"),
-  } as StorePostCartsCartReq
+    email: formData.get('email'),
+  } as StorePostCartsCartReq;
 
-  const sameAsBilling = formData.get("same_as_billing")
+  const sameAsBilling = formData.get('same_as_billing');
 
-  if (sameAsBilling === "on") data.billing_address = data.shipping_address
+  if (sameAsBilling === 'on') data.billing_address = data.shipping_address;
 
-  if (sameAsBilling !== "on")
+  if (sameAsBilling !== 'on')
     data.billing_address = {
-      first_name: formData.get("billing_address.first_name"),
-      last_name: formData.get("billing_address.last_name"),
-      address_1: formData.get("billing_address.address_1"),
-      address_2: "",
-      company: formData.get("billing_address.company"),
-      postal_code: formData.get("billing_address.postal_code"),
-      city: formData.get("billing_address.city"),
-      country_code: formData.get("billing_address.country_code"),
-      province: formData.get("billing_address.province"),
-      phone: formData.get("billing_address.phone"),
-    } as StorePostCartsCartReq
+      first_name: formData.get('billing_address.first_name'),
+      last_name: formData.get('billing_address.last_name'),
+      address_1: formData.get('billing_address.address_1'),
+      address_2: '',
+      company: formData.get('billing_address.company'),
+      postal_code: formData.get('billing_address.postal_code'),
+      city: formData.get('billing_address.city'),
+      country_code: formData.get('billing_address.country_code'),
+      province: formData.get('billing_address.province'),
+      phone: formData.get('billing_address.phone'),
+    } as StorePostCartsCartReq;
 
   try {
-    await updateCart(cartId, data)
-    revalidateTag("cart")
+    await updateCart(cartId, data);
+    revalidateTag('cart');
   } catch (error: any) {
-    return error.toString()
+    return error.toString();
   }
 
-  redirect(`/${lang}/${locale}/checkout?step=delivery`);
+  redirect('checkout?step=delivery');
 }
 
 export async function setShippingMethod(shippingMethodId: string) {
-  const cartId = cookies().get("_medusa_cart_id")?.value
+  const cartId = cookies().get('_medusa_cart_id')?.value;
 
-  if (!cartId) throw new Error("No cartId cookie found")
+  if (!cartId) throw new Error('No cartId cookie found');
 
   try {
-    await addShippingMethod({ cartId, shippingMethodId })
-    revalidateTag("cart")
+    await addShippingMethod({ cartId, shippingMethodId });
+    revalidateTag('cart');
   } catch (error: any) {
-    throw error
+    throw error;
   }
 }
 
 export async function setPaymentMethod(providerId: string) {
-  const cartId = cookies().get("_medusa_cart_id")?.value
+  const cartId = cookies().get('_medusa_cart_id')?.value;
 
-  if (!cartId) throw new Error("No cartId cookie found")
+  if (!cartId) throw new Error('No cartId cookie found');
 
   try {
-    const cart = await setPaymentSession({ cartId, providerId })
-    revalidateTag("cart")
-    return cart
+    const cart = await setPaymentSession({ cartId, providerId });
+    revalidateTag('cart');
+    return cart;
   } catch (error: any) {
-    throw error
+    throw error;
   }
 }
 
 export async function placeOrder() {
-  const cartId = cookies().get("_medusa_cart_id")?.value
+  const cartId = cookies().get('_medusa_cart_id')?.value;
 
-  if (!cartId) throw new Error("No cartId cookie found")
+  if (!cartId) throw new Error('No cartId cookie found');
 
-  let cart
+  let cart;
 
   try {
-    cart = await completeCart(cartId)
-    revalidateTag("cart")
+    cart = await completeCart(cartId);
+    revalidateTag('cart');
   } catch (error: any) {
-    throw error
+    throw error;
   }
 
-  if (cart?.type === "order") {
-    const countryCode = cart.data.shipping_address?.country_code?.toLowerCase()
-    cookies().set("_medusa_cart_id", "", { maxAge: -1 })
-    redirect(`/${countryCode}/order/confirmed/${cart?.data.id}`)
+  if (cart?.type === 'order') {
+    cookies().set('_medusa_cart_id', '', { maxAge: -1 });
+
+    redirect(`order/confirmed/${cart?.data.id}`);
   }
 
-  return cart
+  return cart;
 }
