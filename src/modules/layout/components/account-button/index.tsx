9dd493554ad4be4542bd385d1ceb7e0c2eafdf2b
Nguyen Pham <nguyenfamj1409@gmail.com>
2024-04-25 00:49:49 +0300

    feat: localized account button

--- src/modules/layout/components/account-button/index.tsx
+++ src/modules/layout/components/account-button/index.tsx
@@ -1,12 +1,15 @@
 'use client';
 
+import { useDictionary } from '@lib/context/dictionary-context';
 import LocalizedClientLink from '@modules/common/components/localized-client-link';
 import { memo } from 'react';
 
 const AccountButton = memo(() => {
+  const dictionary = useDictionary();
+
   return (
     <LocalizedClientLink href="/account/order?status=all">
-      Account
+      {dictionary.common.account}
     </LocalizedClientLink>
   );
 });
