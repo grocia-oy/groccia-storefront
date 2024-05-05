516a72e95d0d79413bb1710bbb07f34c009c6b57
Nguyen Pham <nguyenfamj1409@gmail.com>
2024-05-05 17:32:05 +0300

    chore: use dictionary for order management page

--- src/app/[lang]/[locale]/(main)/account/order/page.tsx
+++ src/app/[lang]/[locale]/(main)/account/order/page.tsx
@@ -1,5 +1,6 @@
 import { listCustomerOrders } from '@lib/data/ecommerce';
 import OrderStatusTabs from '@modules/account/components/order-status-tabs';
+import { getDictionary } from 'app/[lang]/dictionaries';
 import { notFound } from 'next/navigation';
 
 type Props = {
@@ -12,13 +13,16 @@ export default async function OrderPage({ params }: Props) {
   const orderResponse = await listCustomerOrders().catch((error) => {
     console.log(error);
   });
+  const dictionary = await getDictionary(params.lang);
 
   if (!orderResponse) return notFound();
 
   return (
     <section className="w-full">
       <div>
-        <h2 className="font-raleway font-bold text-2xl">Order</h2>
+        <h2 className="font-raleway font-bold text-2xl">
+          {dictionary.common.order}
+        </h2>
       </div>
       <OrderStatusTabs orders={orderResponse.orders} />
     </section>
