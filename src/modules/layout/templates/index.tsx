56440acc43bf390c0379943ab34b1c6809e9c5f6
Thang Phan <phancaothng@gmail.com>
2024-05-20 12:08:09 +0300

    fix: make AnnouncementBar component server side

--- src/modules/layout/templates/index.tsx
+++ src/modules/layout/templates/index.tsx
@@ -4,6 +4,7 @@ import Footer from '@modules/layout/templates/footer';
 import Nav from '@modules/layout/templates/nav';
 import FlyoutNav from './flyout-nav';
 import { getCustomer } from '@lib/data/ecommerce';
+import { getGlobalAnnouncementBar } from '@lib/data/content';
 
 const Layout: React.FC<{
   children: React.ReactNode;
@@ -11,11 +12,14 @@ const Layout: React.FC<{
   locale: string;
 }> = async (props) => {
   const customer = await getCustomer().catch(() => null);
+  const content = await getGlobalAnnouncementBar(props.lang).then(
+    (content) => content.data?.announcement_bar
+  );
 
   return (
-    <div lang={props.lang} className='flex flex-col min-h-screen'>
+    <div lang={props.lang} className="flex flex-col min-h-screen">
       <div className="sticky top-0 inset-x-0 z-10">
-        <AnnouncementBar />
+        <AnnouncementBar content={content} />
         <div className="shadow-md">
           <Nav lang={props.lang} locale={props.locale} customer={customer} />
           <FlyoutNav lang={props.lang} locale={props.locale} />
